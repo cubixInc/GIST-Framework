2360a0cd8688b91eeb6b2ab98f214ac0d0d75942
Cubix Labs Inc <shoaib@Shoaibs-MacBook-Pro.local>
2016-07-14 12:31:47 +0500

    Fixing issue for UITabBarController

--- GISTFramework/Classes/BaseClasses/BaseUITabBarController.swift
+++ GISTFramework/Classes/BaseClasses/BaseUITabBarController.swift
@@ -8,15 +8,15 @@
 
 import UIKit
 
-class BaseUITabBarController: UITabBarController {
+public class BaseUITabBarController: UITabBarController {
 
-    override func viewDidLoad() {
+    override public func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
+    override public func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
