060218e17828cc4ea8d58e0ec1dd0eb7efa638d0
Cubix Labs Inc <shoaib@cubixlabs.com>
2016-09-09 15:04:44 +0500

    ValidatedTextField Issue fixed

--- GISTFramework/Classes/GISTCore/ValidatedTextField.swift
+++ GISTFramework/Classes/GISTCore/ValidatedTextField.swift
@@ -3,39 +3,76 @@
 //  Tst1
 //
 //  Created by Shoaib Abdul on 06/09/2016.
-//  Copyright © 2016 Cubix Labs. All rights reserved.
+//  Copyright © 2016 Social Cubix. All rights reserved.
 //
 
 import UIKit
 
+@objc public protocol ValidatedTextFieldDelegate {
+    optional func validatedTextFieldInvalidSignDidTap(validatedTextField:ValidatedTextField, sender:UIButton);
+} //P.E.
+
 public class ValidatedTextField: BaseUITextField {
     
     @IBInspectable var validateEmpty:Bool = false;
+    
     @IBInspectable var validateEmail:Bool = false;
     @IBInspectable var validateURL:Bool = false;
     @IBInspectable var validateNumeric:Bool = false;
     @IBInspectable var validateAlphabetic:Bool = false;
     @IBInspectable var validateRegex:String?;
     
+    @IBInspectable var minChar:Int?;
+    @IBInspectable var maxChar:Int?;
     
-    //??@IBInspectable var continuousValidation:Bool = false;
+    @IBInspectable var invalidSign:UIImage? = nil {
+        didSet {
+            invalidSignBtn.setImage(invalidSign, forState: UIControlState.Normal);
+        }
+    } //P.E.
+    
+    @IBInspectable var validityMsg:String?
+    
+    private lazy var invalidSignBtn:CustomUIButton =  {
+        let cBtn:CustomUIButton = CustomUIButton(type: UIButtonType.Custom);
+        cBtn.hidden = true;
+        cBtn.frame = CGRect(x: self.frame.size.width - self.frame.size.height, y: 0, width: self.frame.size.height, height: self.frame.size.height);
+        cBtn.contentMode = UIViewContentMode.Right;
+        cBtn.containtOffSet = GISTUtility.convertPointToRatio(CGPoint(x: 10, y: 0));
+        
+        cBtn.addTarget(self, action: #selector(invalidSignBtnHandler(_:)), forControlEvents: UIControlEvents.TouchUpInside);
+        
+        self.addSubview(cBtn);
+        return cBtn;
+    } ();
+    
+    private var _isEmpty:Bool = false;
     
     private var _isValid:Bool = false;
     public var isValid:Bool {
         get {
-            return _isValid;
+            let cValid:Bool = (_isValid && (!validateEmpty || !_isEmpty));
+            
+            self.invalidSignBtn.hidden = cValid;
+            
+            return cValid;
         }
     } //F.E.
     
     private func validateText() {
+        _isEmpty = self.isEmpty();
+        
         _isValid =
-            (!validateEmpty || !self.isEmpty()) &&
             (!validateEmail || self.isValidEmail()) &&
             (!validateURL || self.isValidUrl()) &&
             (!validateNumeric || self.isNumeric()) &&
             (!validateAlphabetic || self.isAlphabetic()) &&
+            ((minChar == nil) || self.isValidForMinChar(minChar!)) &&
+            ((maxChar == nil) || self.isValidForMaxChar(maxChar!)) &&
             ((validateRegex == nil) || self.isValidForRegex(validateRegex!));
         
+        
+        self.invalidSignBtn.hidden = (_isValid || _isEmpty);
     } //F.E.
     
     public override func textFieldDidEndEditing(textField: UITextField) {
@@ -43,7 +80,6 @@ public class ValidatedTextField: BaseUITextField {
         //--
         self.validateText();
     } //F.E.
-    
     
     public func isEmpty()->Bool {
         guard (self.text != nil) else {
@@ -95,6 +131,22 @@ public class ValidatedTextField: BaseUITextField {
         return true;
     } //F.E.
     
+    private func isValidForMinChar(noOfChar:Int) -> Bool {
+        guard (self.text != nil) else {
+            return false;
+        }
+        
+        return (self.text!.utf16.count >= noOfChar);
+    } //F.E.
+    
+    private func isValidForMaxChar(noOfChar:Int) -> Bool {
+        guard (self.text != nil) else {
+            return false;
+        }
+        
+        return (self.text!.utf16.count <= noOfChar);
+    } //F.E.
+    
     private func isValidForRegex(regex:String)->Bool {
         guard (self.text != nil) else {
             return false;
@@ -105,4 +157,14 @@ public class ValidatedTextField: BaseUITextField {
         return predicate.evaluateWithObject(self.text!);
     } //F.E.
     
+    public override func layoutSubviews() {
+        super.layoutSubviews();
+        //--
+        self.invalidSignBtn.frame = CGRect(x: self.frame.size.width - self.frame.size.height, y: 0, width: self.frame.size.height, height: self.frame.size.height);
+    } //F.E.
+    
+    
+    func invalidSignBtnHandler(sender:UIButton) {
+        (self.delegate as? ValidatedTextFieldDelegate)?.validatedTextFieldInvalidSignDidTap?(self, sender: sender)
+    } //F.E.
 } //CLS END
