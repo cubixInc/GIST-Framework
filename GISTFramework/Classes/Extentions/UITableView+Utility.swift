312c28ed95a8426e494630c008e31d251798f821
Cubix Labs Inc <shoaib@cubixlabs.com>
2016-12-21 14:48:25 +0500

    Fixed scrolling to bottom issue

--- GISTFramework/Classes/Extentions/UITableView+Utility.swift
+++ GISTFramework/Classes/Extentions/UITableView+Utility.swift
@@ -28,17 +28,18 @@ public extension UITableView {
     
     public func scrollToBottom(_ animated: Bool = true) {
         
-        let numOfSections = self.numberOfSections;
-        guard (self.numberOfSections != 0) else {
-            return;
-        }
+        let delay = 0.1 * Double(NSEC_PER_SEC)
         
-        let numOfRows = self.numberOfRows(inSection: self.numberOfSections - 1);
-        guard (numOfRows != 0) else {
-            return;
+        DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
+            let numberOfSections = self.numberOfSections
+            let numberOfRows = self.numberOfRows(inSection: numberOfSections-1)
+            
+            if numberOfRows > 0 {
+                let indexPath = IndexPath(row: numberOfRows-1, section: (numberOfSections-1));
+                
+                self.scrollToRow(at: indexPath, at: UITableViewScrollPosition.bottom, animated: animated)
+            }
         }
-        
-        self.scrollToRow(at: IndexPath(row: numOfRows - 1, section: numOfSections - 1), at: .bottom, animated: animated)
     } //F.E.
     
     public func scrollToTop(_ animated: Bool = true) {
