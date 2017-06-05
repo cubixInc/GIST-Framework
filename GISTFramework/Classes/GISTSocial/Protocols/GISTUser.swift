2a1d02276f3ad03d27e2afe5e538e640dae344e8
Cubix Labs Inc <shoaib@cubixlabs.com>
2017-06-05 18:07:03 +0500

    Fixed mobile number issue

--- GISTFramework/Classes/GISTSocial/Protocols/GISTUser.swift
+++ GISTFramework/Classes/GISTSocial/Protocols/GISTUser.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import ObjectMapper
+import PhoneNumberKit
 
 /*
 {"kick_user":0,"data":{"user":{"user_id":134,"type":"user","name":"","first_name":"","last_name":"","user_name":null,"email":"","dob":null,"gender":"male","image":"","thumb":"","status":1,"platform_type":"facebook","platform_id":"76543212","device_udid":"","device_type":"ios","mobile_no":"","city_id":null,"country_id":null,"state_id":null,"zip_code":"","device_token":"","is_verified":1,"mobile_verified_at":null,"is_email_verified":0,"verification_token":null,"is_mobile_verified":0,"sent_email_verification":0,"sent_mobile_verification":0,"is_guest":0,"additional_note":null,"other_data":null,"remember_login_token_created_at":null,"last_login_at":"2017-05-25 07:45:24","last_seen_at":null,"created_at":"2017-05-25 07:42:16","updated_at":null},"client_token":"XapP@#_xw-Oof295807e082fdb158578918241b5213e"},"message":"Success","error":0}
@@ -38,3 +39,15 @@ public protocol GISTUser: Mappable, ReverseMappable, NSCopying {
     
     init();
 } //P.E.
+
+extension GISTUser {
+    var formatedMobileNo:String? {
+        get {
+            if let pNumber:PhoneNumber = GISTUtility.validatePhoneNumber(self.mobileNo) {
+                return "+\(pNumber.countryCode)-\(pNumber.nationalNumber)";
+            }
+            
+            return nil;
+        }
+    }
+}
