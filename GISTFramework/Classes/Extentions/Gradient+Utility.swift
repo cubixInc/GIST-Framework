a9cbd3db609687f5d5bd9892cfc3f67b7a6ab82c
Shoaib <shoaib@cubixlabs.com>
2018-02-05 17:29:37 +0500

    Added Gradient Helper

---
+++ GISTFramework/Classes/Extentions/Gradient+Utility.swift
@@ -0,0 +1,78 @@
+//
+//  GradientUtility.swift
+//  GistDev
+//
+//  Created by Shoaib on 2/5/18.
+//  Copyright © 2018 Social Cubix. All rights reserved.
+//
+
+import UIKit
+
+public extension CAGradientLayer {
+    
+    public func setGradient(for type:GradientType) {
+        let points = type.points();
+        
+        self.startPoint = points.start;
+        self.endPoint = points.end;
+    } //F.E.
+    
+} //E.E.
+
+public enum GradientType {
+    case leftRight
+    case rightLeft
+    case topBottom
+    case bottomTop
+    case topLeftBottomRight
+    case bottomRightTopLeft
+    case topRightBottomLeft
+    case bottomLeftTopRight
+    
+    fileprivate func points() -> (start: CGPoint, end: CGPoint) {
+        switch self {
+        case .leftRight:
+            return (start: CGPoint(x: 0, y: 0.5), end: CGPoint(x: 1, y: 0.5))
+        case .rightLeft:
+            return (start: CGPoint(x: 1, y: 0.5), end: CGPoint(x: 0, y: 0.5))
+        case .topBottom:
+            return (start: CGPoint(x: 0.5, y: 0), end: CGPoint(x: 0.5, y: 1))
+        case .bottomTop:
+            return (start: CGPoint(x: 0.5, y: 1), end: CGPoint(x: 0.5, y: 0))
+        case .topLeftBottomRight:
+            return (start: CGPoint(x: 0, y: 0), end: CGPoint(x: 1, y: 1))
+        case .bottomRightTopLeft:
+            return (start: CGPoint(x: 1, y: 1), end: CGPoint(x: 0, y: 0))
+        case .topRightBottomLeft:
+            return (start: CGPoint(x: 1, y: 0), end: CGPoint(x: 0, y: 1))
+        case .bottomLeftTopRight:
+            return (start: CGPoint(x: 0, y: 1), end: CGPoint(x: 1, y: 0))
+        }
+    }
+    
+    public  static func gradientType(for type:String) -> GradientType {
+        switch type {
+        case "leftRight":
+            return .leftRight
+        case "rightLeft":
+            return .rightLeft
+        case "topBottom":
+            return .topBottom
+        case "bottomTop":
+            return .bottomTop
+        case "topLeftBottomRight":
+            return .topLeftBottomRight
+        case "bottomRightTopLeft":
+            return .bottomRightTopLeft
+        case "topRightBottomLeft":
+            return .topRightBottomLeft
+        case "bottomLeftTopRight":
+            return .bottomLeftTopRight
+        default:
+            return .leftRight
+        }
+        
+    } //F.E.
+} //E.E.
+
+
