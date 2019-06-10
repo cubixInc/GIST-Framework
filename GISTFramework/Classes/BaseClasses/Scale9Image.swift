e3ab6e0948cccf9a58abdb6d971a8902582495b3
Shoaib <shoaib@cubixlabs.com>
2019-06-10 16:52:32 +0500

    Fixed syntax error

--- GISTFramework/Classes/BaseClasses/Scale9Image.swift
+++ GISTFramework/Classes/BaseClasses/Scale9Image.swift
@@ -7,16 +7,15 @@
 //
 
 import UIKit
-import GISTFramework
 
 open class Scale9Image: BaseUIImageView {
     
-    @IBInspectable open var edgeInset:Float = 20;
+    @IBInspectable open var edgeInset:CGFloat = 20;
 
     open override func awakeFromNib() {
         super.awakeFromNib();
         
-        self.image = self.image?.resizableImage(withCapInsets: UIEdgeInsets(top: edgeInset, left: edgeInset, bottom: edgeInset, right: edgeInset), resizingMode: UIImageResizingMode.stretch);
+        self.image = self.image?.resizableImage(withCapInsets: UIEdgeInsets(top: edgeInset, left: edgeInset, bottom: edgeInset, right: edgeInset), resizingMode: UIImage.ResizingMode.stretch);
     }
 
 } //CLS END
